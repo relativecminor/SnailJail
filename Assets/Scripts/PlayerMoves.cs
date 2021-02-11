016e0496df25a87c9bbf75c63821bfb9db300f17
Cameron Minor <minorcj@hendrix.edu>
2021-02-10 22:50:43 -0600

    Changed Gale to prefab and added SnailTrail

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerMoves : MonoBehaviour
 {
     Rigidbody2D body;
+    SpriteRenderer spriteRend;
 
     private float horizontal;
     private float vertical;
+
+    private float moveLimiter = 0.7f;
 
     public float runSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,5 +30,19 @@ public class PlayerMoves : MonoBehaviour
     private void FixedUpdate()
     {
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+        
+        if (horizontal == -1)
+        {
+            spriteRend.flipX = true;
+        }
+        else if (horizontal == 1)
+        {
+            spriteRend.flipX = false;
+        }
     }
 }
